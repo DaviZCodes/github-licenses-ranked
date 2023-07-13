8d331f288e20ac3c6a65241aabf6f18f097cc735
DaviZCodes <davi.bestguy@gmail.com>
2023-07-13 18:49:40 -0400

    added tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
+      <header className="flex justify-between mb-6">
       <h1 className="text-3xl font-semibold">GitHub Licenses Simplified</h1>
+      </header>
+
+      <footer>
+        <div className="flex justify-center gap-3">
+          <Link className= "text-stone-600 underline text-base" href = "/definitions"> GitHub</Link>
+          <Link className= "text-stone-600 underline text-base" href = "/definitions"> Definitions </Link>
+        </div>
+      </footer>
     </main>
   )
 }
