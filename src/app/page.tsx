ff476f50c6265df40ebec5d4ca0502325fd0063d
DaviZCodes <davi.bestguy@gmail.com>
2023-07-13 16:50:13 -0400

    tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>GitHub Licenses Simplified</h1>
+      <h1 className="text-3xl font-semibold">GitHub Licenses Simplified</h1>
     </main>
   )
 }
