76e86a1729376d069afdd60ac99b5575035940f3
DaviZCodes <davi.bestguy@gmail.com>
2023-07-23 16:58:42 -0400

    changes

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,13 +4,16 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <header>
+    <div className="flex items-center">
       <Link className="flex mb-6" href="/">
         <Image src={logo} width={150} height={150} alt="logo"></Image>
         <h1 className="text-3xl font-semibold text-blue-[550]">
           GitHub Licenses Simplified
         </h1>
       </Link>
-    </header>
+      <Link className="text-stone-600 underline text-base" href="/definitions">
+        Definitions
+      </Link>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center gap-3">
-      <Link
-        className="text-stone-600 underline text-base"
-        href="https://github.com/DaviZCodes/github-licenses-simplified"
-        target="_blank"
-      >
-        GitHub
-      </Link>
-      <Link className="text-stone-600 underline text-base" href="/definitions">
-        Definitions
-      </Link>
-    </div>
-  );
+  return <div className="flex justify-center gap-3"></div>;
 }
