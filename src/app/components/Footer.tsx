298cedfe1fd5a49921e1b888885152be89465fdd
DaviZCodes <davi.bestguy@gmail.com>
2023-07-23 16:57:00 -0400

    added navbar

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import { BsGithub } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <div>
       <BsGithub />
+      <p>
+        Developed by{" "}
+        <Link href="https://github.com/DaviZCodes"> DaviZCodes</Link>
+      </p>
     </div>
   );
 }
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import logo from "./images/licenses logo.png";
+import Image from "next/image";
+
 export default function NavBar() {
   return (
-    <header className="flex justify-between mb-6">
-      <h1 className="text-3xl font-semibold">GitHub Licenses Simplified</h1>
+    <header>
+      <Link className="flex mb-6" href="/">
+        <Image src={logo} width={150} height={150} alt="logo"></Image>
+        <h1 className="text-3xl font-semibold text-blue-[550]">
+          GitHub Licenses Simplified
+        </h1>
+      </Link>
     </header>
   );
 }
