6c90ea2f98c1d922d8c4dd9a37046407f2b13f59
DaviZCodes <davi.bestguy@gmail.com>
2023-07-31 16:10:37 -0400

    cursor pointer and image

--- src/app/components/LicenseCard.tsx
+++ src/app/components/LicenseCard.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
+
 interface LicenseCardProps {
   name: string;
 }
 
 const LicenseCard: React.FC<LicenseCardProps> = ({ name }) => {
-  return <div className="border bg-custom-blue">{name}</div>;
+  return (
+    <div className="border bg-custom-blue cursor-pointer">
+      <Image
+        src={
+          "https://drive.google.com/uc?export=view&id=14vrGk8NhuNqxFrL_y3GlBsKskkchXOfe"
+        }
+        width={100}
+        height={100}
+        alt="License image"
+      />
+      {name}
+    </div>
+  );
 };
 
 export default LicenseCard;
