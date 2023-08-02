94f69470d86055f9c80c7487735992ad0c01ab7a
DaviZCodes <davi.bestguy@gmail.com>
2023-08-01 21:21:09 -0400

    added image

--- src/app/components/LicenseCard.tsx
+++ src/app/components/LicenseCard.tsx
@@ -10,7 +10,16 @@ const LicenseCard: React.FC<LicenseCardProps> = ({ name, path }) => {
   return (
     <div className="border bg-custom-blue cursor-pointer">
       <Link href={`license/${path}`}>
-        <Image src={""} width={100} height={100} alt={`${name} image`} />
+        <div>
+          <Image
+            src={
+              "https://i.pinimg.com/736x/b4/06/dd/b406dd9617bde411b761778c5c99cfc2--nerd-metal.jpg"
+            }
+            width={100}
+            height={100}
+            alt={`${name} image`}
+          />
+        </div>
         {name}
       </Link>
     </div>
