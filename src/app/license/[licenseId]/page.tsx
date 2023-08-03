245476d0bb6c4f447c6c55ffa8f0c8d2b656e884
DaviZCodes <davi.bestguy@gmail.com>
2023-08-03 12:54:31 -0400

    license params

--- src/app/license/[licenseId]/page.tsx
+++ src/app/license/[licenseId]/page.tsx
@@ -1,3 +1,9 @@
+interface LicensePageProps {
+  params: {
+    licenseName: string;
+  };
+}
+
 export default function SingleLicensePage() {
   return <div className="min-h-screen">Single License Page</div>;
 }
