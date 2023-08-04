84b8709e0088fd8d797268d2a9973d016d9eb69b
DaviZCodes <davi.bestguy@gmail.com>
2023-08-03 21:58:15 -0400

    code fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const onSortChange = () => {};
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="min-h-screen">
       <SortDropdown onSortChange={onSortChange} />
       <div className="grid grid-cols-1 gap-3 w-full h-96 md:grid-cols-2 lg:grid-cols-3 2xl:gap-5">
         {licensesObj.map((license) => (
