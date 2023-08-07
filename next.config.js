f0d8aa94bdc7222d9cb06fa07bbe507946adb4f7
DaviZCodes <davi.bestguy@gmail.com>
2023-08-07 17:06:01 -0400

    added domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["drive.google.com", "static.vecteezy.com"],
+    domains: [
+      "drive.google.com",
+      "static.vecteezy.com",
+      "upload.wikimedia.org",
+      "assets-prod.sumo.prod.webservices.mozgcp.net",
+      "pm1.aminoapps.com",
+      "media.cnn.com",
+      "arthive.net",
+    ],
   },
 };
 
