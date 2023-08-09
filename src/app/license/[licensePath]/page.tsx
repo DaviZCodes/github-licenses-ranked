c9717fc76cc3f72bdfe84a65a9c05656b07661b8
DaviZCodes <davi.bestguy@gmail.com>
2023-08-09 16:36:34 -0400

    added single license page

--- src/app/license/[licensePath]/page.tsx
+++ src/app/license/[licensePath]/page.tsx
@@ -1,4 +1,5 @@
 import { licensesObj } from "@/app/constants";
+import Image from "next/image";
 
 interface LicensePageProps {
   params: {
@@ -9,10 +10,42 @@ interface LicensePageProps {
 export default function SingleLicensePage({ params }: LicensePageProps) {
   const { licensePath } = params; //get the URL path
 
+  const desiredPath = "/" + licensePath;
+
+  const desiredLicense = licensesObj.find(
+    (license) => license.path === desiredPath
+  );
+
+  if (!desiredLicense) {
+    return (
+      <div className="font-bold text-red-600">
+        Error 404. Failed to find License. Please try another license.
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen text-center">
-      <div className="text-xl sm:text-2xl font-semibold">{licensePath}</div>
-      <div>Source:</div>
+      <div className="text-xl sm:text-2xl font-semibold">
+        {desiredLicense.name}
+      </div>
+      <div className="w-96">
+        <Image
+          src={desiredLicense.image}
+          width={0}
+          height={0}
+          sizes="100vh"
+          alt="License image"
+          className="w-full h-full"
+        />
+      </div>
+      <div className="flex flex-col">
+        <p>{desiredLicense.description}</p>
+        <p>Strictness Rank: {desiredLicense.strictness}</p>
+        <p>Strictness Points: {desiredLicense.strictness_points}</p>
+        <p>Popularity rank: {desiredLicense.popularity}</p>
+      </div>
+      <div>Source: {desiredLicense.src}</div>
     </div>
   );
 }
