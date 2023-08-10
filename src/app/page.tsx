b4648a0c900cf637c4fac5f7efa22d07975a7809
DaviZCodes <davi.bestguy@gmail.com>
2023-08-09 23:52:00 -0400

    fixed dropdown sort

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
 "use client";
+import { useState } from "react";
 import LicenseCard from "./components/LicenseCard";
 import SortDropdown from "./components/SortDropdown";
 import { licensesObj } from "./constants";
 
-const onSortChange = () => {};
-
 export default function Home() {
+  const [sortedLicenses, setSortedLicenses] = useState([...licensesObj]);
+
+  const onSortChange = (selectedOption: string) => {
+    let sortedLicenses = [...licensesObj];
+
+    if (selectedOption === "most-popular") {
+      sortedLicenses.sort(
+        (a, b) => (a.popularity as number) - (b.popularity as number)
+      );
+    } else if (selectedOption === "least-popular") {
+      sortedLicenses.sort(
+        (a, b) => (b.popularity as number) - (a.popularity as number)
+      );
+    } else if (selectedOption === "most-strict") {
+      sortedLicenses.sort(
+        (a, b) => (a.strictness as number) - (b.strictness as number)
+      );
+    } else if (selectedOption === "least-strict") {
+      sortedLicenses.sort(
+        (a, b) => (b.strictness as number) - (a.strictness as number)
+      );
+    }
+
+    setSortedLicenses(sortedLicenses);
+  };
+
   return (
     <div className="min-h-screen">
       <div className="flex flex-col items-center sm:flex sm:flex-row gap-3">
@@ -15,7 +40,7 @@ export default function Home() {
         </div>
       </div>
       <div className="grid grid-cols-1 gap-2 w-full md:grid-cols-2 lg:grid-cols-3 2xl:gap-3">
-        {licensesObj.map((license) => (
+        {sortedLicenses.map((license) => (
           <LicenseCard
             key={license.name}
             name={license.name}
